8ba8a7477f928141be5f648315fa97bbd193a3e0
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-08-10 09:44:47 -0300

    Adiciona uso de um .env para configurar porta do servidor

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
+from dotenv import load_dotenv
 
 from model import BANCO_DE_DADOS
 
 app = Flask(__name__)
+_ = load_dotenv()
 
 @app.route("/")
 def home() -> str:
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    port = int(os.environ.get("PORTA_SERVIDOR", 5000))
+
+    app.run(host="0.0.0.0", debug=True, port=port)
