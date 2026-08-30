35c7b4ffe340d5759eb375111ac1bf14bbedfde0
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-08-30 11:28:41 -0300

    Adiciona rotas do backend e suas respectivas páginas HTML

--- app.py
+++ app.py
@@ -8,6 +8,57 @@ from model import BANCO_DE_DADOS
 app = Flask(__name__)
 _ = load_dotenv()
 
+
+# Alunos =======================================================================
+
+@app.route("/alunos/")
+def alunos() -> str:
+    return render_template("alunos.html")
+
+@app.route("/alunos/novo/")
+def novo_aluno() -> str:
+    return render_template("novo_aluno.html")
+
+# ==============================================================================
+
+# Professores ==================================================================
+
+@app.route("/professores/")
+def professores() -> str:
+    return render_template("professores.html")
+
+@app.route("/professores/novo/")
+def novo_professor() -> str:
+    return render_template("novo_professor.html")
+
+# ==============================================================================
+
+# Matérias =====================================================================
+
+@app.route("/materias/")
+def materias() -> str:
+    return render_template("materias.html")
+
+@app.route("/materias/nova")
+def nova_materia() -> str:
+    return render_template("nova_materia.html")
+
+# ==============================================================================
+
+# Cursos =======================================================================
+
+@app.route("/cursos/")
+def cursos() -> str:
+    return render_template("cursos.html")
+
+@app.route("/cursos/novo")
+def novo_curso() -> str:
+    return render_template("novo_curso.html")
+
+# ==============================================================================
+
+
+
 @app.route("/")
 def home() -> str:
     return render_template("index.html")
