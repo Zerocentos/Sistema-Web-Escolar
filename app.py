cf44115c05132c046075abae13331247ed464de0
Zerocentos <a2025950912@teiacoltec.org>
2026-09-02 21:49:10 -0300

    feat: Adiciona página para mostrar um professor
    Essa página é acessada página da tabela dos professores, clicando em
    cima do professor(a) que deseja, irá ver as informações dele mais
    detalhadamente.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 from datetime import datetime
 from flask import Flask, render_template, request
@@ -11,6 +10,7 @@ from model import BANCO_DE_DADOS
 app = Flask(__name__)
 _ = load_dotenv()
 
+#TODO: Apagar os comentários das bibliotecas
 
 # Util ========================================================================
 
@@ -161,360 +161,32 @@ def novo_aluno() -> str:
 # ==============================================================================
 
 # Professores ==================================================================
-
-@app.route("/professores/")
-def professores() -> str:
-    conn = sqlite3.connect(BANCO_DE_DADOS)
-    cursor = conn.cursor()
-
-    professores: list[list[str]] = list(map(
-        list, cursor.execute("""
-                            SELECT id, nome, email, telefone, email
-                            FROM professor """).fetchall()
-    ))
-
-    for professor in professores:
-        _ = cursor.execute("""
-        SELECT materia.nome FROM materia_professor
-        JOIN materia 
-        ON materia.id_materia = materia.
-        WHERE matricula_aluno = ?
-        """, (int(professor[0]),))
-
-        rows: list[tuple[str]] = list(cursor.fetchall())
-        nome_materia = rows[0][0]
-
-        professor.insert(2, nome_materia)
-
-    conn.close()
-    
-    return render_template("professores.html", Professores=professores)
-
-@app.route("/professores/novo/", methods=["GET", "POST"])
-def novo_professor() -> str:  
-    conn = sqlite3.connect(BANCO_DE_DADOS)
-    cursor = conn.cursor()
-
-    _ = cursor.execute("SELECT id_materia, nome FROM materia")
-    
-    materias = cursor.fetchall()
-
-    if request.method == "POST":
-        nome = request.form["nome"].strip()
-        email = request.form["email"].strip()
-        telefone = request.form["telefone"].strip()
-        data_nascimento = request.form["data-nascimento"]
-        id_materia = request.form["materia"]
-
-        _ = cursor.execute('''
-        INSERT INTO professor (nome, email, telefone, data_nascimento)
-        VALUES (?, ?, ?, ?)
-        ''', (nome, email, telefone, data_nascimento))
-
-        id_professor = cursor.lastrowid
-
-        _ = cursor.execute('''
-        INSERT INTO materia_professor (id_materia, id_professor)
-        VALUES (?, ?)
-        ''', (id_materia, id_professor))
-
-        conn.commit()
-        conn.close()
-
-        return render_template("novo_aluno.html",
-                                Mensagem="Aluno cadastrado com sucesso!", Materias=materias)
-
-    conn.close()
-    
-    return render_template("novo_professor.html", Materias=materias)
-
-# ==============================================================================
-
-# Matérias =====================================================================
-
-@app.route("/materias/")
-def materias() -> str:
-    cursor = sqlite3.connect(BANCO_DE_DADOS).cursor()
-
-    materias = cursor.execute("SELECT * FROM materia").fetchall()
-
-    return render_template("materias.html", 
-                           Materias = materias)
-
-@app.route("/materias/nova/", methods=["GET", "POST"])
-def nova_materia() -> str:
-    if request.method == "POST":
-        nome = request.form["nome"].strip()
-        carga_horaria = request.form["carga_horaria"]
-
-        conn = sqlite3.connect(BANCO_DE_DADOS)
-        cursor = conn.cursor()
-        
-        _ = cursor.execute(
-            "INSERT INTO materia (nome, carga_horaria) VALUES (?, ?)",
-            (nome, carga_horaria)
-        ) 
-          
-        conn.commit()
-        conn.close()
-        
-        return render_template('nova_materia.html', 
-                             Mensagem=f"Matéria cadastrada com sucesso!")
- 
-    return render_template("nova_materia.html", Mensagem="")
-
-# ==============================================================================
-
-# Cursos =======================================================================
-
-@app.route("/cursos/")
-def cursos() -> str:
-    conn = sqlite3.connect(BANCO_DE_DADOS)
-    cursor = conn.cursor()
-
-    _ = cursor.execute("""
-    SELECT id_curso, nome, descricao FROM curso
-    """)
-
-    _cursos: list[tuple[str]] = cursor.fetchall()
-    cursos = list(map(list, _cursos))
-
-    for curso in cursos:
-        id_curso = curso[0]
-
-        _ = cursor.execute("""
-        SELECT materia.nome FROM curso_materia 
-        JOIN materia ON materia.id_materia
-        WHERE curso_materia.id_curso = ?
-        """, (id_curso,))
-
-        rows: list[list[str]] = cursor.fetchall()
-        
-        materias = [str(row[0]) for row in rows]
-
-        curso.append(", ".join(materias))
-
-    return render_template("cursos.html", Cursos=cursos)
-
-@app.route("/cursos/novo/", methods=["GET", "POST"])
-def novo_curso() -> str:
-    if request.method == "POST":
-        nome = request.form["nome"].strip()
-        descricao = request.form["descricao"].strip()
-        materias = request.form.getlist("materias")
-
-        conn = sqlite3.connect(BANCO_DE_DADOS)
-        cursor = conn.cursor()
-        
-        _ = cursor.execute(
-            "INSERT INTO curso (nome, descricao) VALUES (?, ?)",
-            (nome, descricao)
-        ) 
-
-        id = cursor.lastrowid
-
-        for id_materia in materias:
-            _ = cursor.execute("""
-            INSERT INTO curso_materia (id_curso, id_materia)
-            VALUES (?, ?)
-            """, (id, id_materia))
-        
-        conn.commit()
-        conn.close()
-        
-        return render_template('novo_curso.html', 
-                             Mensagem="Curso cadastrado com sucesso!")
-
-
-    conn = sqlite3.connect(BANCO_DE_DADOS)
-    cursor = conn.cursor()
-
-    _ = cursor.execute("SELECT id_materia, nome FROM materia")
-
-    materias = cursor.fetchall()
-
-    conn.close()
-
-    return render_template("novo_curso.html", Materias=materias)
-
-# ==============================================================================
-
-
-
-@app.route("/")
-def home() -> str:
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    if not os.path.exists(BANCO_DE_DADOS):
-        print("""
-Não foi possível encontrar o arquivo do banco de dados, impossível iniciar o servidor.
-
-Para criar o banco de dados, rode 'model.py'.
-        """)
-        sys.exit();
-
-    port = int(os.environ.get("PORTA_SERVIDOR", 5000))
-    app.run(host="0.0.0.0", debug=True, port=port)
-=======
-import sys
-from datetime import datetime
-from flask import Flask, render_template, request
-import sqlite3
-import os
-from dotenv import load_dotenv
-
-from model import BANCO_DE_DADOS
-
-app = Flask(__name__)
-_ = load_dotenv()
-
-
-# Util ========================================================================
-
-def formatar_data(data) -> str:
-    d =  datetime.strptime(data, "%Y-%m-%d")
-    return d.strftime("%d/%m/%Y")
-
-# Alunos =======================================================================
-
-# Coloca zeros à esquerda na matricula se necessário
-def formatar_matricula(matricula) -> str:
-    return f"{int(matricula):04d}"
-
-@app.route("/alunos/<int:matricula>/")
-def aluno(matricula: int) -> str:   
+@app.route("/professores/<int:id_professor>/")
+def professor(id_professor: int) -> str:   
     conn = sqlite3.connect(BANCO_DE_DADOS)
     cursor = conn.cursor() 
 
     _ = cursor.execute("""
-    SELECT * FROM aluno
-    WHERE matricula = ?
-    """, (matricula,))
+    SELECT * FROM professor
+    WHERE id_professor = ?
+    """, (id_professor,))
 
-    aluno = list(cursor.fetchone())
+    professor = list(cursor.fetchone())
 
     _ = cursor.execute("""
-    SELECT curso.nome FROM aluno_curso
-    JOIN curso ON curso.id_curso = aluno_curso.id_curso
-    WHERE matricula_aluno = ?
-    """, (int(aluno[0]),))
+    SELECT materia.nome FROM materia_professor
+    JOIN materia ON materia.id_materia = materia_professor.id_materia
+    WHERE id_professor = ?
+    """, (int(professor[0]),))
 
     row: list[str] = list(cursor.fetchone())
-    nome_curso = row[0]
+    nome_materia = row[0]
 
-    aluno.append(nome_curso)
+    professor.append(nome_materia)
 
-    aluno[0] = formatar_matricula(aluno[0])
-    aluno[4] = formatar_data(aluno[4])
+    professor[4] = formatar_data(professor[4])
 
-    return render_template("aluno.html", Aluno=aluno)
-
-@app.route("/alunos/")
-def alunos() -> str:
-    conn = sqlite3.connect(BANCO_DE_DADOS)
-    cursor = conn.cursor()
-
-    alunos: list[list[str]] = list(map(
-        list, cursor.execute("SELECT matricula, nome, data_nascimento FROM aluno").fetchall()
-    ))
-
-    for aluno in alunos:
-        aluno[0] = formatar_matricula(aluno[0]) 
-        _ = cursor.execute("""
-        SELECT curso.nome FROM aluno_curso
-        JOIN curso ON curso.id_curso = aluno_curso.id_curso
-        WHERE matricula_aluno = ?
-        """, (int(aluno[0]),))
-
-        rows: list[tuple[str]] = list(cursor.fetchall())
-
-        nome_curso = rows[0][0]
-        aluno.insert(2, nome_curso)
-
-        data = formatar_data(aluno[3])
-        aluno[3] = data 
-
-    conn.close()
-
-    return render_template("alunos.html", Alunos=alunos)
-
-@app.route("/alunos/novo/", methods=["GET", "POST"])
-def novo_aluno() -> str:
-    conn = sqlite3.connect(BANCO_DE_DADOS)
-    cursor = conn.cursor()
-
-    if request.method == "POST":
-        nome = request.form["nome"].strip()
-        email = request.form["email"].strip()
-        telefone = request.form["telefone"].strip()
-        data_nascimento = request.form["data-nascimento"]
-        id_curso = request.form["curso"]
-
-        _ = cursor.execute('''
-        INSERT INTO aluno (nome, email, telefone, data_nascimento)
-        VALUES (?, ?, ?, ?)
-        ''', (nome, email, telefone, data_nascimento))
-
-        matricula = cursor.lastrowid
-
-        _ = cursor.execute('''
-        INSERT INTO aluno_curso (id_curso, matricula_aluno)
-        VALUES (?, ?)
-        ''', (id_curso, matricula))
-
-        conn.commit()
-        conn.close()
-
-        return render_template("novo_aluno.html",
-                               Mensagem="Aluno cadastrado com sucesso!")
-
-    _ = cursor.execute("SELECT id_curso, nome FROM curso")
-
-    cursos = cursor.fetchall()
-
-    conn.close()
-
-    return render_template("novo_aluno.html", Cursos=cursos)
-
-# ==============================================================================
-
-# Matérias =====================================================================
-
-@app.route("/materias/")
-def materias() -> str:
-    cursor = sqlite3.connect(BANCO_DE_DADOS).cursor()
-
-    materias = cursor.execute("SELECT * FROM materia").fetchall()
-
-    return render_template("materias.html", 
-                           Materias = materias)
-
-@app.route("/materias/nova/", methods=["GET", "POST"])
-def nova_materia() -> str:
-    if request.method == "POST":
-        nome = request.form["nome"].strip()
-        carga_horaria = request.form["carga_horaria"]
-
-        conn = sqlite3.connect(BANCO_DE_DADOS)
-        cursor = conn.cursor()
-        
-        _ = cursor.execute(
-            "INSERT INTO materia (nome, carga_horaria) VALUES (?, ?)",
-            (nome, carga_horaria)
-        ) 
-          
-        conn.commit()
-        conn.close()
-        
-        return render_template('nova_materia.html', 
-                             Mensagem=f"Matéria cadastrada com sucesso!")
- 
-    return render_template("nova_materia.html", Mensagem="")
-
-# ==============================================================================
-
-# Professores ==================================================================
+    return render_template("professor.html", Professor=professor)
 
 @app.route("/professores/")
 def professores() -> str:
@@ -523,7 +195,7 @@ def professores() -> str:
 
     professores: list[list[str]] = list(map(
         list, cursor.execute("""
-                            SELECT id, nome, email, telefone, email
+                            SELECT id_professor, nome, email, telefone, data_nascimento
                             FROM professor """).fetchall()
     ))
 
@@ -531,14 +203,14 @@ def professores() -> str:
         _ = cursor.execute("""
         SELECT materia.nome FROM materia_professor
         JOIN materia 
-        ON materia.id_materia = materia.
-        WHERE matricula_aluno = ?
+        ON materia.id_materia = materia_professor.id_materia
+        WHERE id_professor = ?
         """, (int(professor[0]),))
 
         rows: list[tuple[str]] = list(cursor.fetchall())
         nome_materia = rows[0][0]
 
-        professor.insert(2, nome_materia)
+        professor.append(nome_materia)
 
     conn.close()
     
@@ -575,12 +247,47 @@ def novo_professor() -> str:
         conn.commit()
         conn.close()
 
-        return render_template("novo_aluno.html",
-                                Mensagem="Aluno cadastrado com sucesso!", Materias=materias)
+        return render_template("novo_professor.html",
+                                Mensagem="Professor cadastrado com sucesso!", Materias=materias)
 
     conn.close()
     
     return render_template("novo_professor.html", Materias=materias)
+
+# ==============================================================================
+
+# Matérias =====================================================================
+
+@app.route("/materias/")
+def materias() -> str:
+    cursor = sqlite3.connect(BANCO_DE_DADOS).cursor()
+
+    materias = cursor.execute("SELECT * FROM materia").fetchall()
+
+    return render_template("materias.html", 
+                           Materias = materias)
+
+@app.route("/materias/nova/", methods=["GET", "POST"])
+def nova_materia() -> str:
+    if request.method == "POST":
+        nome = request.form["nome"].strip()
+        carga_horaria = request.form["carga_horaria"]
+
+        conn = sqlite3.connect(BANCO_DE_DADOS)
+        cursor = conn.cursor()
+        
+        _ = cursor.execute(
+            "INSERT INTO materia (nome, carga_horaria) VALUES (?, ?)",
+            (nome, carga_horaria)
+        ) 
+          
+        conn.commit()
+        conn.close()
+        
+        return render_template('nova_materia.html', 
+                             Mensagem=f"Matéria cadastrada com sucesso!")
+ 
+    return render_template("nova_materia.html", Mensagem="")
 
 # ==============================================================================
 
@@ -675,4 +382,3 @@ Para criar o banco de dados, rode 'model.py'.
 
     port = int(os.environ.get("PORTA_SERVIDOR", 5000))
     app.run(host="0.0.0.0", debug=True, port=port)
->>>>>>> 110b985 (fix?: Tentando consertar erro de merge)
