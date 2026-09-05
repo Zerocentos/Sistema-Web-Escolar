d48c55942338cc82b605cc6714cfbe38cdac66d2
Zerocentos <a2025950912@teiacoltec.org>
2026-09-05 11:20:16 -0300

    Faz checagem de colunas

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ BANCO_DE_DADOS = "Sistema-Web-Escolar.db"
 
 def ativa_foreign_key() -> str:
     query = '''
-    PRAGMA foreign_key = ON
+    PRAGMA foreign_keys = ON
     '''
 
     return query
@@ -13,8 +13,8 @@ def tabela_aluno() -> str:
     query = '''
     CREATE TABLE IF NOT EXISTS aluno(
         matricula INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome VARCHAR(60) NOT NULL, 
-        email VARCHAR(40) NOT NULL,
+        nome VARCHAR(60) NOT NULL CHECK (LENGTH(nome) <= 60), 
+        email VARCHAR(40) NOT NULL UNIQUE,
         telefone VARCHAR(30) NOT NULL,
         data_nascimento DATETIME
     )
@@ -25,8 +25,8 @@ def tabela_materia() -> str:
     query = '''
     CREATE TABLE IF NOT EXISTS materia(
         id_materia INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome VARCHAR(40) NOT NULL,
-        carga_horaria INT NOT NULL  
+        nome VARCHAR(40) NOT NULL CHECK (LENGTH(nome) <= 40),
+        carga_horaria INT NOT NULL CHECK (carga_horaria > 0)
     )
     '''
     return query
@@ -35,8 +35,8 @@ def tabela_curso() -> str:
     query = '''
     CREATE TABLE IF NOT EXISTS curso(
         id_curso INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome VARCHAR(50) NOT NULL,
-        descricao VARCHAR(40) 
+        nome VARCHAR(50) NOT NULL CHECK (LENGTH(nome) <= 50),
+        descricao VARCHAR(100) CHECK (LENGTH(descricao) <= 100)
     )
     '''
 
@@ -46,8 +46,8 @@ def tabela_professor() -> str:
     query = '''
     CREATE TABLE IF NOT EXISTS professor(
         id_professor INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome VARCHAR(60) NOT NULL, 
-        email VARCHAR(40) NOT NULL,
+        nome VARCHAR(60) NOT NULL CHECK (LENGTH(nome) <= 60), 
+        email VARCHAR(40) NOT NULL UNIQUE,
         telefone VARCHAR(30) NOT NULL,
         data_nascimento DATETIME
     )
@@ -61,7 +61,7 @@ def aluno_curso() -> str:
         matricula_aluno INTEGER NOT NULL,
         PRIMARY KEY (id_curso, matricula_aluno),
         FOREIGN KEY (matricula_aluno) REFERENCES aluno(matricula) ON DELETE CASCADE,
-        FOREIGN KEY (id_curso) REFERENCES curso(id) ON DELETE CASCADE
+        FOREIGN KEY (id_curso) REFERENCES curso(id_curso) ON DELETE CASCADE
     )
     '''
     return query
@@ -71,10 +71,10 @@ def aluno_materia() -> str:
     CREATE TABLE IF NOT EXISTS aluno_materia(
         matricula_aluno INTEGER NOT NULL,
         id_materia INTEGER NOT NULL,
-        nota INTEGER NOT NULL,
+        nota INTEGER NOT NULL CHECK (nota >= 0 AND nota <= 100),
         PRIMARY KEY (matricula_aluno, id_materia),
         FOREIGN KEY (matricula_aluno) REFERENCES aluno(matricula) ON DELETE CASCADE,
-        FOREIGN KEY (id_materia) REFERENCES materia(id) ON DELETE CASCADE 
+        FOREIGN KEY (id_materia) REFERENCES materia(id_materia) ON DELETE CASCADE 
     )
     '''
     return query
@@ -97,8 +97,8 @@ def materia_professor() -> str:
         id_professor INTEGER NOT NULL,
         id_materia INTEGER NOT NULL,
         PRIMARY KEY (id_professor, id_materia),
-        FOREIGN KEY (id_materia) REFERENCES materia(id) ON DELETE CASCADE,
-        FOREIGN KEY (id_professor) REFERENCES professor(id) ON DELETE CASCADE
+        FOREIGN KEY (id_materia) REFERENCES materia(id_materia) ON DELETE CASCADE,
+        FOREIGN KEY (id_professor) REFERENCES professor(id_professor) ON DELETE CASCADE
     )
     '''
     return query
